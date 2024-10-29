65947eb26d4891bd21d086da24976a32872ad857
Pang Teck Hong <23036686@myrp.edu.sg>
2024-10-29 16:31:17 +0800

    push my code

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState}from "react";
+import { StyleSheet, Text, View,TextInput, Button, Alert, TouchableOpacity, ToastAndroid } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
-export default function App() {
+const MyApp = () => {
+    const [userType, setUserType] = useState('');
+    const [userName, setUserName] = useState('');
+
+    const handleLogin = () => {
+        if (userType && userName) {
+            const message = `Welcome ${userType === 'admin' ? 'Admin' : 'User'} ${userName}`;
+            ToastAndroid.show(message, ToastAndroid.LONG);
+        } else {
+            ToastAndroid.show("Please fill in all fields", ToastAndroid.SHORT);
+        }
+    };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <View>
+        <Text></Text>
+        <Text></Text>
+      <Text>User Type:</Text>
+        <RNPickerSelect
+            onValueChange={(value) => setUserType(value)}
+            items={[
+                { label: 'User', value : 'user'},
+                { label: 'Admin', value : 'admin'},
+            ]}
+        />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        <Text>User Name:</Text>
+        <TextInput style={{borderWidth:1}} onChangeText={(text) => setUserName(text)}/>
+
+        <Text>Password:</Text>
+      <TextInput style={{borderWidth: 1}} />
+
+        <Text></Text>
+        <Text></Text>
+
+        {/*### 1D ###*/}
+        {/*<Button*/}
+        {/*    onPress={() => {Alert.alert("Welcome!")}}*/}
+        {/*    title="Log In"*/}
+        {/*/>*/}
+
+        {/*### 1E ###*/}
+        {/*<TouchableOpacity onPress={() => Alert.alert("Welcome!")}>*/}
+        {/*    <Text>Log In</Text>*/}
+        {/*</TouchableOpacity>*/}
+
+        <TouchableOpacity onPress={handleLogin}>
+            <Text>Log In</Text>
+        </TouchableOpacity>
+    </View>
+
+  );
+};
+
+export default MyApp;
